fec6bec3a941d11d89344d67c1b651cf2c9b441e
H2n <hoangngochoa1406@gmail.com>
2020-07-13 19:09:24 +0700

    anh o

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -4,4 +4,6 @@ class Dish < ApplicationRecord
   belongs_to :category
   has_many :comments
   has_many :booking_dishes
+  default_scope -> { order(created_at: :desc) }
+  validates :category_id , presence: true
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 class Menu < ApplicationRecord
-  has_many :dishes
-  has_many :menu_dishes
+	has_many :menu_dishes
 end
